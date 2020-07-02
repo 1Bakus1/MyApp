ac0bdff2c7bc1a60665dbd72b8406137229266ee
Mateusz Bakun <bakun.mateusz2303@gmail.com>
2020-07-02 21:04:39 +0200

    Dodanie logowania

---
+++ MyApp.API/Models/User.cs
@@ -0,0 +1,10 @@
+namespace MyApp.API.Models
+{
+    public class User
+    {
+           public int Id { get; set; }
+           public string Usernam { get; set; }
+           public byte[] PasswordHash { get; set; }
+           public byte[] PasswordSalt { get; set; }
+    }
+}
